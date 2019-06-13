8ee65d2dea008668d48cfd443fec856a6f5f8e82
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-06-13 16:21:46 -0300

    menu items styles

--- src/components/Menu/styles.js
+++ src/components/Menu/styles.js
@@ -1,16 +1,32 @@
+import { StyleSheet } from 'react-native';
 import styled from 'styled-components/native';
 
-export const Container = styled.ScrollView.attrs({
-  contentContainerStyle: { alignItems: 'center' },
-})`
+export const Container = styled.ScrollView`
   margin: 0 30px;
 `;
 
 export const Code = styled.View`
+  align-self: center;
   background: #fff;
   padding: 10px;
 `;
 
-export const Nav = styled.View``;
-export const NavItem = styled.View``;
-export const NavText = styled.Text``;
+export const Nav = styled.View`
+  border-top-color: rgba(255, 255, 255, 0.8);
+  border-top-width: ${StyleSheet.hairlineWidth}px;
+  margin-top: 30px;
+`;
+
+export const NavItem = styled.View`
+  align-items: center;
+  border-bottom-color: rgba(255, 255, 255, 0.8);
+  border-bottom-width: ${StyleSheet.hairlineWidth}px;
+  flex-direction: row;
+  padding: 12px 0;
+`;
+
+export const NavText = styled.Text`
+  color: #fff;
+  font-size: 15px;
+  margin-left: 20px;
+`;
