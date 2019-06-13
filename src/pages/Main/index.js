e1f18f07abc380fb0e5b10399692d3d9e5dc86be
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-06-13 16:07:23 -0300

    added card styles

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,14 +1,46 @@
 import React from 'react';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 
 import Header from '~/components/Header';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="#666" />
+            <Icon name="visibility-off" size={28} color="#666" />
+          </CardHeader>
+
+          <CardContent>
+            <Title>Saldo disponível</Title>
+            <Description>R$ 16.123,65</Description>
+          </CardContent>
+
+          <CardFooter>
+            <Annotation>
+              Transferência de R$ 659,12 recebida de Vinícius Catafesta Francisco
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
--- src/pages/Main/styles.js
+++ src/pages/Main/styles.js
@@ -4,5 +4,55 @@ import { getStatusBarHeight } from 'react-native-iphone-x-helper';
 export const Container = styled.View`
   flex: 1;
   background: #8b10ae;
+  justify-content: center;
   padding-top: ${getStatusBarHeight()}px;
+`;
+
+export const Content = styled.View`
+  flex: 1;
+  max-height: 400px;
+  z-index: 5;
+`;
+
+export const Card = styled.View`
+  background: #fff;
+  border-radius: 4px;
+  flex: 1;
+  height: 100%;
+  margin: 0 20px;
+`;
+
+export const CardHeader = styled.View`
+  align-items: center;
+  flex-direction: row;
+  justify-content: space-between;
+  padding: 30px;
+`;
+
+export const CardContent = styled.View`
+  flex: 1;
+  justify-content: center;
+  padding: 0 30px;
+`;
+
+export const Title = styled.Text`
+  color: #999;
+  font-size: 13px;
+`;
+
+export const Description = styled.Text`
+  color: #333;
+  font-size: 32px;
+  margin-top: 3px;
+`;
+
+export const CardFooter = styled.View`
+  background: #eee;
+  border-radius: 4px;
+  padding: 30px;
+`;
+
+export const Annotation = styled.Text`
+  color: #333;
+  font-size: 12px;
 `;
