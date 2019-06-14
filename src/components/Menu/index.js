f5ab16af68ec369f401c8bb2bbcf242c2f5af39d
Vinicius Catafesta Francisco <vini.ibi@gmail.com>
2019-06-14 08:51:52 -0300

    fixed Menu structure

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,5 +1,4 @@
 import React from 'react';
-
 import QRCode from 'react-native-qrcode';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
@@ -7,7 +6,7 @@ import {
   Container, Code, Nav, NavItem, NavText, SignOutButtom, SignOutButtomText,
 } from './styles';
 
-export default function Menu() {
+function Menu() {
   return (
     <Container>
       <Code>
@@ -24,14 +23,17 @@ export default function Menu() {
           <Icon name="help-outline" size={20} color="#FFF" />
           <NavText>Me ajuda</NavText>
         </NavItem>
+
         <NavItem>
           <Icon name="person-outline" size={20} color="#FFF" />
           <NavText>Perfil</NavText>
         </NavItem>
+
         <NavItem>
           <Icon name="credit-card" size={20} color="#FFF" />
           <NavText>Configurar cartão</NavText>
         </NavItem>
+
         <NavItem>
           <Icon name="smartphone" size={20} color="#FFF" />
           <NavText>Configurações do app</NavText>
@@ -44,3 +46,5 @@ export default function Menu() {
     </Container>
   );
 }
+
+export default Menu;
